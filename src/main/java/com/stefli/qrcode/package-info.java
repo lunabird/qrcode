9854a4bd7f0248c48b16cf68a3db60bc5e8a4db2
lunabird <xd_huangpeng@163.com>
2016-02-03 09:56:14 +0800

    added the logo test.

---
+++ src/main/java/com/stefli/qrcode/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2015, Powered by stefli. All Rights Reserved. 
+ */
+/**
+ * @author stefli
+ * @version 1.0
+ * @date 2015年10月31日 下午1:03:46
+ *
+ */
+package com.stefli.qrcode;
---
+++ src/test/java/com/stefli/qrcode/QRCodeKitTest.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2015, Powered by stefli. All Rights Reserved. 
+ */
+package com.stefli.qrcode;
+
+import static org.junit.Assert.assertEquals;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import org.junit.Test;
+
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+/**
+ * @author stefli
+ * @version 1.0
+ * @date 
+ *
+ */
+public class QRCodeKitTest {
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Test
+	public void testGenerateWithOnlyData() throws IOException, NotFoundException {
+		String data = "http://www.baidu.com";
+		BufferedImage image = QRCodeKit.createQRCode(data);
+		ImageIO.write(image, "png", new File("result.png"));
+		Map hint = new HashMap();
+		hint.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+		hint.put(EncodeHintType.MARGIN, 1);
+		hint.put(EncodeHintType.CHARACTER_SET, QRCodeKit.QRCODE_DEFAULT_CHARSET);
+		String result = QRCodeKit.readQRCode("result.png", hint);
+		assertEquals(data, result);
+	}
+
+	@Test
+	public void testBase64Encode() throws IOException {
+		System.out.println("testBase64Encode()\n\n\n");
+		String data = "http://www.baidu.com";
+		BufferedImage image = QRCodeKit.createQRCode(data);
+		String result = QRCodeKit.getImageBase64String(image);
+		System.out.println(result);
+		QRCodeKit.convertBase64StringToImage(result, new File("decode.png"));
+		System.out.println("###############################################\n\n\n");
+	}
+	
+	@Test
+	public void testBase64EncodeWithLogo() throws IOException {
+		System.out.println("testBase64EncodeWithLogo()\n\n\n");
+		String data = "http://www.baidu.com";
+		File logoFile = new File("Koala_logo.png");
+		BufferedImage image = QRCodeKit.createQRCodeWithLogo(data, logoFile);
+		String result = QRCodeKit.getImageBase64String(image);
+		System.out.println(result);
+		QRCodeKit.convertBase64StringToImage(result, new File("decode1.png"));
+		System.out.println("###############################################\n\n\n");
+	}
+}
